f322164af0dc59d39a58f2f2663f04d148c503b8
Cenk Şengünlü <cenksengunlu@hotmail.com>
2022-03-15 00:24:06 +0300

    initial commit 🎉

---
+++ src/components/PokepediaComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PokepediaComponent = () => {
+    return(
+      <h1 className="text-3xl font-bold underline">
+        Poképedia!
+      </h1>
+    )
+}
+
+export default PokepediaComponent;
